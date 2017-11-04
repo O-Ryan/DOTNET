3f1e6867b1820c7a9b860811ddecba09eac2664c
O-Ryan <orrisryan@gmail.com>
2017-11-04 00:30:00 -0400

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Write your Javascript code.
+function ThisFunction(id, person) {
+
+}
+console.log('');
+
+// Something, Something, Something, Darkside...
+function name(){
+    return value;
+}
